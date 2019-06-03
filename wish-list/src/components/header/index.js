2b0cb3d9d72e677e73463c8aabb9d8f47d0bfe46
Anton Romankov <romant094@gmail.com>
2019-06-03 18:35:29 +0300

    Lesson 6: optional. Declination and no dependency from case.

--- wish-list/src/components/header/index.js
+++ wish-list/src/components/header/index.js
@@ -20,10 +20,20 @@ const HeaderBlock = styled.div`
 `;
 
 const Header = ({liked, allPosts}) => {
+
+    const declination = (allPosts) => {
+        const n = allPosts;
+        let result;
+
+        if ((n === 1) || (n > 20 && n % 10 === 1)) result = 'запись';
+        else if ((n >= 2 && n <= 4) || (n > 20 && n % 10 >= 2 && n % 10 <= 4)) result = 'записи';
+        else result = 'записей';
+        return result;
+    };
     return (
         <HeaderBlock>
             <h1>Anton Romankov</h1>
-            <h2>{allPosts} записей, понравилось {liked}</h2>
+            <h2>{allPosts} {declination(allPosts)}, понравилось {liked}</h2>
         </HeaderBlock>
     )
 };
