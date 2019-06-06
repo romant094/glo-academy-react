af6af70244e95451cc0af4625651afd639ac7860
Anton Romankov <romant094@gmail.com>
2019-06-06 17:35:35 +0300

    Lesson 8: main. Added button to show/hide random char

--- react-gotapp/src/components/app/app.js
+++ react-gotapp/src/components/app/app.js
@@ -1,34 +1,51 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import React, {Component} from 'react';
+import {Col, Row, Container, Button} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
-};
+export default class App extends Component {
+    state = {
+        charVisible: true
+    };
 
-export default App;
+    charVisibleToggle = () => {
+        this.setState((state) => ({
+            charVisible: !state.charVisible
+        }))
+    };
+
+    render() {
+        return (
+            <>
+                <Container>
+                    <Header/>
+                    <Button color={'primary'}
+                            onClick={this.charVisibleToggle}
+                            className={'mx-auto mb-3'}>
+                        Show / hide char
+                    </Button>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            <div className={this.state.charVisible ? null : 'd-none'}>
+                                <RandomChar/>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList/>
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails/>
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        )
+    }
+}
