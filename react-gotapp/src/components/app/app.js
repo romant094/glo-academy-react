ba2570b27a0c80e7ae12429bc9e1bac51f7effc4
Anton Romankov <romant094@gmail.com>
2019-06-06 17:14:02 +0300

    Lesson 8: main. Data transform

--- react-gotapp/src/components/app/app.js
+++ react-gotapp/src/components/app/app.js
@@ -4,17 +4,9 @@ import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
-import GotService from '../../service';
 
 
 const App = () => {
-    // TODO Delete this check
-    console.log((new GotService).getAllCharacters());
-    console.log((new GotService).getCharacter(1));
-    console.log((new GotService).getAllHouses());
-    console.log((new GotService).getHouse(1));
-    console.log((new GotService).getAllBooks());
-    console.log((new GotService).getBook(1));
     return (
         <> 
             <Container>
