c4a34d0a6d854314a50c4bd5230a24209e0d27b3
Anton_Romankov <a.romankov@fil-it.ru>
2019-07-19 12:13:41 +0300

    Lesson 15: optional. Added redirect from home page

--- react-resto-app/src/components/app/app.js
+++ react-resto-app/src/components/app/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import {MainPage, CartPage, MenuItemPage} from '../pages';
 import AppHeader from '../app-header';
 
@@ -25,6 +25,7 @@ const App = () => {
                     return <MenuItemPage id={id}/>;
                 }}/>
                 <Route path='/cart' render={() => <CartPage/>}/>
+                <Redirect from='/' to='/menu-items'/>
             </Switch>
         </div>
     )
